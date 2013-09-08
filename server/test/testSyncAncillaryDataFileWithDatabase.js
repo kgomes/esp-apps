ab6d402468c82618a1ed961fee25f4ea63d238f2
Kevin Gomes <kgomes@mbari.org>
2013-09-07 22:50:33 -0700

    Adding a small test to check CSV file building.  Has to be configured through config.js and doesn't really evaluate anything, just builds the CSV files which can then be viewed.

--- server/test/testSyncAncillaryDataFileWithDatabase.js
+++ server/test/testSyncAncillaryDataFileWithDatabase.js
@@ -1,7 +1,32 @@
-/**
- * Created with JetBrains WebStorm.
- * User: kgomes
- * Date: 9/7/13
- * Time: 5:15 PM
- * To change this template use File | Settings | File Templates.
- */
+// Read in the ESP config file
+var espCfg = require('../config.js');
+// Import 3rd party library dependencies
+var log4js = require('log4js');
+
+// Configure the logger to use a file appender
+log4js.loadAppender('file');
+log4js.addAppender(log4js.appenders.file('./logs/testSyncAncillaryDataFileWithDatabase.log'), 'test');
+
+// Grab the logger
+var logger = log4js.getLogger('test');
+
+// Log the configuration at startup
+logger.info('Staring testAncillaryDataFileWithDatabase with options: ', espCfg);
+
+// Create the object used to interface to the data stores
+var da = require('../DataAccess').createDataAccess(espCfg.dataStoreOptions);
+
+// The directory where the esp applications and data live
+var espDataDir = espCfg.dataDir;
+
+// Use the dataAccess to find a deployment
+var deploymentToUse = da.getDeploymentByID('37a96082d1c94ca581303c90c300091e', function (err, deployment) {
+    logger.debug('Deployment that we are using is ' +  deployment.name + ' of ESP ' + deployment.esp.name);
+    // Call the method to sync
+    da.syncAncillaryDataFileWithDatabase(deployment, espDataDir, function(err, result) {
+        if (err){
+            logger.error("ERROR trying to sync data to file: ", err);
+        }
+        logger.debug("Sync done, result: ", result);
+    });
+});
