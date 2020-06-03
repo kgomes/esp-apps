2a417d305cb3d7b4d123e2c4197c5c2b4ef86b74
Kevin Gomes <kgomes@mbari.org>
2020-06-03 10:28:29 -0700

    Changed sleep and added line to start crond

--- run.sh
+++ run.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+# Sleep to allow the other services to start
+echo "Sleeping 30 seconds to allow other services to start"
+sleep 30s
+
+# Start the crond service
+crond &
+
 # Start the FTP file crawler
 echo "Starting FTP crawler"
 cd /opt/esp/server
